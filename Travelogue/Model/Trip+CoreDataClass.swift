65aacf3c16e141d0e465a1a6f9b469b2b51c8cf4
Dylan Mouser <codecruncher@mwc-054112.dhcp.missouri.edu>
2019-05-10 19:20:33 -0500

    Core Data error fixed

--- Travelogue/Model/Trip+CoreDataClass.swift
+++ Travelogue/Model/Trip+CoreDataClass.swift
@@ -9,8 +9,18 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 @objc(Trip)
 public class Trip: NSManagedObject {
 
+    convenience init?(name: String?) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        guard let managedContext = appDelegate?.persistentContainer.viewContext,
+            let name = name, name != "" else {
+                return nil
+        }
+        self.init(entity: Trip.entity(), insertInto: managedContext)
+        self.title = name
+    }
 }
